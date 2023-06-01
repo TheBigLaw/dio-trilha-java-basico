0d6e2e7ca1ddde62d35896724e210f15b59b4161
Andre <andre_ananiasmarques@outlook.com>
2023-05-31 23:30:45 -0300

    Aula operadores

---
+++ Operadores/src/operadores_aula/operadores_aula.java
@@ -0,0 +1,14 @@
+package operadores_aula;
+
+
+public class operadores_aula {
+    
+    public static void main (String [] args){
+
+        String nomeCompleto = "LINGUAGEM" + " " + "JAVA";
+        System.out.println(nomeCompleto);
+
+
+    }
+
+}
