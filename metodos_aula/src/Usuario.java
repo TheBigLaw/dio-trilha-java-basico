9bc69c33ca39868b928e66cda89211f14f480d4d
Andre <andre_ananiasmarques@outlook.com>
2023-06-02 00:00:07 -0300

    Changes

--- metodos_aula/src/Usuario.java
+++ metodos_aula/src/Usuario.java
@@ -1,19 +1,22 @@
 public class Usuario {
+    
     public static void main(String[] args) throws Exception {
 
         SmartTv smartTv = new SmartTv();
+
+        smartTv.ligar();
+        System.out.println ("TV esta ligada? " + smartTv.ligada);
         
-        System.out.println ("TV esta ligada? " + smartTv.ligada);
-        System.out.println ("Canal: " + smartTv.canal);
-        System.out.println ("Volume: " + smartTv.volume);
-
         smartTv.aumentarVolume();
-        smartTv.subirCanal();
-
-        System.out.println ("TV esta ligada? " + smartTv.ligada);
-        System.out.println ("Canal: " + smartTv.canal);
         System.out.println ("Volume: " + smartTv.volume);
-
-
+        smartTv.aumentarVolume();
+        System.out.println ("Volume: " + smartTv.volume);
+        smartTv.aumentarVolume();
+        System.out.println ("Volume: " + smartTv.volume);
+        smartTv.subirCanal();
+        System.out.println ("Canal: " + smartTv.canal);
+        smartTv.subirCanal();
+        System.out.println ("Canal: " + smartTv.canal);
+        
     }
 }
